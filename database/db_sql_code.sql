f1a4ba5093a75801f64c7a636b03c2afaeb071f8
Claudio Parra <101138305+cdp50@users.noreply.github.com>
2023-02-11 21:32:13 -0700

    activity done class view

--- database/db_sql_code.sql
+++ database/db_sql_code.sql
@@ -237,3 +237,10 @@ VALUES   (
     'White',
     5
   );
+
+UPDATE
+    public.inventory
+SET 
+    inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+	inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
+
